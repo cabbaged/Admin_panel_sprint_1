a19692bdd3103b7ee6eaf75b72c11963251337e5
Gleb Chebykin <chebykingi@gmail.com>
2021-07-18 11:57:18 +0300

    Style corrections

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,12 +1,11 @@
-import sqlite3
-
-import psycopg2
-from psycopg2.extensions import connection as _connection
-from psycopg2.extras import DictCursor, NamedTupleCursor, execute_batch
-
-import uuid
-from dataclasses import dataclass, field, asdict, astuple
 import datetime
+import psycopg2
+import sqlite3
+import uuid
+
+from dataclasses import dataclass, field, asdict, astuple
+from psycopg2.extensions import connection as _connection
+from psycopg2.extras import DictCursor, execute_batch
 
 
 @dataclass(frozen=True)
@@ -109,7 +108,6 @@ class PostgresSaver:
         )
 
 
-
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     """Основной метод загрузки данных из SQLite в Postgres"""
     postgres_saver = PostgresSaver(pg_conn)
@@ -119,12 +117,12 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     postgres_saver.save_all_data(data)
 
 
-if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database',
-           'user': 'postgres',
-           'password': 1234,
-           'host': '127.0.0.1',
-           'port': 5432,
-           'options': '-c search_path=content'}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+if __name__ == "__main__":
+    dsl = {"dbname": "movies_database",
+           "user": "postgres",
+           "password": 1234,
+           "host": "127.0.0.1",
+           "port": 5432,
+           "options": "-c search_path=content"}
+    with sqlite3.connect("db.sqlite") as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
