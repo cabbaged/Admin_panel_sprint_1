2d398c6ca9ede567856edbbb5fa30c85c3fe383c
Gleb Chebykin <chebykingi@gmail.com>
2021-07-27 10:33:50 +0300

    Actualize settings

--- movies_admin/config/settings/dev.py
+++ movies_admin/config/settings/dev.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DEBUG = True
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'movies',
+    'django_extensions'
+]
